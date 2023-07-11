b0cf09b3153c1ad12bcb6234d9a3fc1c863ff141
Chris Oliver <excid3@gmail.com>
2023-07-11 09:44:44 -0500

    First commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,44 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: [:edit, :update, :destroy]
+
+  def index
+    @todos = Todo.all
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      redirect_to root_path
+    else
+      @todos = Todo.all
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @todo.update(todo_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @todo.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title)
+  end
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+end
