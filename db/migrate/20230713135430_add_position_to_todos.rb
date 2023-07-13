7c44ae4c8c0fef445a450cd1020d8fb27255e2bf
Chris Oliver <excid3@gmail.com>
2023-07-13 09:26:26 -0500

    Sort todos by completed_at before setting positions

--- db/migrate/20230713135430_add_position_to_todos.rb
+++ db/migrate/20230713135430_add_position_to_todos.rb
@@ -2,7 +2,7 @@ class AddPositionToTodos < ActiveRecord::Migration[7.0]
   def change
     add_column :todos, :position, :integer
 
-    Todo.find_each.with_index do |todo, index|
+    Todo.order(completed_at: :desc).find_each.with_index do |todo, index|
       todo.update_column :position, index
     end
   end
